ddbd32c0015ed8c56266cb9cb1c0ef54671571d7
mdsnn <mwikosinyangwe@gmail.com>
2026-05-11 22:24:42 +0200

    kkk

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,6 +22,79 @@ func getTodos(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }
 
+func createTodo(c *gin.Context) {
+	var input struct {
+		Title string `json:"title" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	todo := Todo{
+		ID:    nextID,
+		Title: input.Title,
+		Done:  false,
+	}
+
+	todos = append(todos, todo)
+	nextID++
+
+	c.JSON(http.StatusCreated, todo)
+}
+
+func updateTodo(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
+
+	var input struct {
+		Title string `json:"title"`
+		Done  *bool  `json:"done"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.ID == id {
+			if input.Title != "" {
+				todos[i].Title = input.Title
+			}
+			if input.Done != nil {
+				todos[i].Done = *input.Done
+			}
+			c.JSON(http.StatusOK, todos[i])
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+}
+
+func deleteTodo(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		return
+	}
+
+	for i, todo := range todos {
+		if todo.ID == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			c.JSON(http.StatusOK, gin.H{"message": "todo deleted"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+}
+
 func main() {
 	r := gin.Default()
 
@@ -33,6 +107,9 @@ func main() {
 	})
 
 	r.GET("/todos", getTodos)
+	r.POST("/todos", createTodo)
+	r.PUT("/todos/:id", updateTodo)
+	r.DELETE("/todos/:id", deleteTodo)
 
 	r.Run(":8080")
 }
