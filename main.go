3e91e675867711c97f7b90874d3ade36e6ecc608
mdsnn <mwikosinyangwe@gmail.com>
2026-05-11 16:28:32 +0200

    kkk

--- main.go
+++ main.go
@@ -6,20 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var (
+	todos  = []Todo{}
+	nextID = 1
+)
+
+func getTodos(c *gin.Context) {
+	c.JSON(http.StatusOK, todos)
+}
+
 func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello, World!",
-		})
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"pong": true,
-		})
+		c.JSON(http.StatusOK, gin.H{"pong": true})
 	})
 
-	r.Run(":8080") // listens on http://localhost:8080
+	r.GET("/todos", getTodos)
+
+	r.Run(":8080")
 }
